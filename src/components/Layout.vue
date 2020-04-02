ceb3d9e6aa70c07f5dfc075178218f4c0f94022b
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-02 22:48:11 +0200

    Add seeder button.

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,34 +2,29 @@
   <v-app id="archaeology-companion">
     <v-app-bar app clipped-left color="amber">
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <span class="title ml-3 mr-5">Archaeology&nbsp;<span class="font-weight-light">companion</span></span>
-      <v-text-field
-        solo-inverted
-        flat
-        hide-details
-        label="Search..."
-        prepend-inner-icon="search"
-      />
+      <span class="title ml-3 mr-5">
+        Archaeology&nbsp;
+        <span class="font-weight-light">companion</span>
+      </span>
+      <v-text-field solo-inverted flat hide-details label="Search..." prepend-inner-icon="search" />
 
       <v-spacer />
+
+      <v-btn v-on:click="seed()">
+        <v-icon style="margin-right: .3rem">refresh</v-icon>
+        Re-seed
+      </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-      color="grey lighten-4"
-    >
+    <v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
       <v-list nav class="grey lighten-4">
         <template v-for="(item, i) in items">
-          <v-list-item :key="i" link :to="item.route" >
+          <v-list-item :key="i" link :to="item.route">
             <v-list-item-action>
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title class="grey--text text--darken-1">
-                {{ item.text }}
-              </v-list-item-title>
+              <v-list-item-title class="grey--text text--darken-1">{{ item.text }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </template>
@@ -41,25 +36,33 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-      items: [
-        { text: 'Materials'  , icon: 'category'       , route: '/materials' },
-        { text: 'Artefacts'  , icon: 'gradient'       , route: '/artefacts' },
-        { text: 'Dig sites'  , icon: 'account_balance', route: '/dig-sites' },
-        { text: 'Excavations', icon: 'gavel'          , route: '/excavations' },
-        { text: 'Collections', icon: 'view_quilt'     , route: '/collections' },
-      ],
-    }),
+import Seeder from '@/seeder';
+
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { text: 'Materials', icon: 'category', route: '/materials' },
+      { text: 'Artefacts', icon: 'gradient', route: '/artefacts' },
+      { text: 'Dig sites', icon: 'account_balance', route: '/dig-sites' },
+      { text: 'Excavations', icon: 'gavel', route: '/excavations' },
+      { text: 'Collections', icon: 'view_quilt', route: '/collections' },
+    ],
+  }),
+  methods: {
+    seed() {
+      const seeder = new Seeder(this.$store);
+      seeder.seed();
+    }
   }
+}
 </script>
 
 <style>
 #keep .v-navigation-drawer__border {
-  display: none
+  display: none;
 }
 </style>
