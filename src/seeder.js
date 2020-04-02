bfea322328056d0c8003db1ef7d045960a727bb9
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-02 22:48:00 +0200

    Add seeder.

--- src/seeder.js
+++ src/seeder.js
@@ -1,4 +1,5 @@
 import Material from '@/models/material';
+import faker from 'faker';
 
 export default class Seeder {
     constructor(store) {
@@ -6,10 +7,14 @@ export default class Seeder {
     }
 
     seed() {
-
+        this.seedMaterials();
     }
 
     seedMaterials() {
+        this.$store.dispatch('materials/reset');
 
+        for (let i = 0; i < 10; i++) {
+            this.$store.dispatch('materials/add', new Material(faker.random.word(), faker.random.number({min:1, max:120})));
+        }
     }
 }
