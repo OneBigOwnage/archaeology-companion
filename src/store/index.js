ac048d0c1d9bc8a5dc9832008a269ab1b1abd668
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-02 14:56:00 +0200

    Add store persistence.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import materials from './materials';
+import VuexPersistence from 'vuex-persist';
 
 Vue.use(Vuex)
+
+const persistence = new VuexPersistence({
+  storage: window.localStorage
+}).plugin;
 
 export default new Vuex.Store({
   modules: {
     materials,
   },
+  plugins: [persistence]
 });
