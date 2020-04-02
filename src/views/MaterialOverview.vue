61252c9e885f485529d3e6c748c021a1318d4db8
Niek <niekvandenbos@hotmail.com>
2020-04-02 12:32:12 +0200

    Improve material overview.

--- src/views/MaterialOverview.vue
+++ src/views/MaterialOverview.vue
@@ -1,10 +1,36 @@
 <template>
-  <h1>This is the material overview</h1>
+  <v-container fluid>
+    <h1>This is the material overview</h1>
+
+    <v-row dense>
+      <v-col v-for="(material, i) in this.$store.state.materials.items" :key="i" cols="2">
+        <v-card>
+          <v-card-title>{{ material.name }}</v-card-title>
+          <v-card-text class="text--primary">
+            <div>Level: {{ material.level }}</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-btn v-on:click="add()">Add items</v-btn>
+
+  </v-container>
 </template>
 
 <script>
+import Material from './../models/material';
+
 export default {
   name: 'MaterialOverview',
+  methods: {
+    add() {
+      this.$store.dispatch('materials/add', new Material('Mat 1', 1));
+      this.$store.dispatch('materials/add', new Material('Mat 2', 2));
+      this.$store.dispatch('materials/add', new Material('Mat 3', 3));
+      this.$store.dispatch('materials/add', new Material('Mat 4', 4));
+    }
+  }
 }
 </script>
 
