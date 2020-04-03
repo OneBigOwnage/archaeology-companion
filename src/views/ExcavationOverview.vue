4ffd29b2e01b2f49b55534484c5c341b4cb759f0
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-03 21:17:33 +0200

    Add excavation views.

--- src/views/ExcavationOverview.vue
+++ src/views/ExcavationOverview.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>This is the excavation overview</h1>
+  <v-container>
+    <h1>Excavations</h1>
+
+    <v-row>
+      <v-col v-for="(excavation, i) in this.$store.state.excavations.all" :key="i" cols="3" class="overview-column">
+        <v-hover v-slot:default="{ hover }">
+          <v-card v-on:click="detailView(excavation)" :elevation="hover ? 12 : 2" :class="{ 'on-hover': hover }" class="overview-card">
+            <v-card-title>{{ excavation.name }}</v-card-title>
+            <v-card-text>
+              Level {{ excavation.level }}
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
