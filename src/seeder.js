36d89e03e2314e0aba86e5d82ec760aaeef82a6c
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-03 13:04:22 +0200

    Add artefact seeding.

--- src/seeder.js
+++ src/seeder.js
@@ -1,4 +1,6 @@
 import Material from '@/models/material';
+import Artefact from '@/models/artefact';
+import { sluggify } from '@/helpers';
 import faker from 'faker';
 
 export default class Seeder {
@@ -8,6 +10,7 @@ export default class Seeder {
 
     seed() {
         this.seedMaterials();
+        this.seedArtefacts();
     }
 
     seedMaterials() {
@@ -15,6 +18,21 @@ export default class Seeder {
 
         for (let i = 0; i < 10; i++) {
             this.$store.dispatch('materials/add', new Material(faker.random.word(), faker.random.number({min:1, max:120})));
+        }
+    }
+
+    seedArtefacts() {
+        this.$store.dispatch('artefacts/reset');
+
+        for (let i = 0; i < 10; i++) {
+            const artefact = new Artefact(
+                faker.random.word(),
+                faker.random.number({ min: 600, max: 20000 }),
+                faker.random.number({ min: 100, max: 1000 }),
+                sluggify(faker.random.words(3))
+            );
+
+            this.$store.dispatch('artefacts/add', artefact);
         }
     }
 }
