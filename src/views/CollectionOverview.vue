914f59a7f8e346298438f25e4fba959ed3e41bff
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-04 09:16:09 +0200

    Add collection overview.

--- src/views/CollectionOverview.vue
+++ src/views/CollectionOverview.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>This is the collection overview</h1>
+  <v-container>
+    <h1>Collections</h1>
+
+    <v-row>
+      <v-col v-for="(collection, i) in this.$store.state.collections.all" cols="3" :key="i" class="overview-column">
+        <v-hover v-slot:default="{ hover }">
+          <v-card v-on:click="detailView(collection)" class="overview-card" :elevation="hover ? 12 : 2" :class="{ 'on-hover': hover }">
+            <v-card-title>{{ collection.name }}</v-card-title>
+            <v-card-text>
+              <div>NPC: {{ collection.NPCName }}</div>
+              <div>Rewards: {{ collection.rewards }}</div>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'CollectionOverview',
+  methods: {
+    detailView(collection) {
+      this.$router.push({ name: 'collection-details', params: { slug: collection.slug() } });
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
