c6a1776893b168a96e575f09641ae26bb77a1bc9
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-04 15:42:33 +0200

    Move styles to assets.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from '@/router';
 import vuetify from '@/plugins/vuetify';
 import store from '@/store'
 import '@/components';
-import '@/styles.css';
+import '@/assets/styles.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
