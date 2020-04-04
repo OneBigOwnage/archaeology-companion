e3cdeb6057c8de3eaf2ea53d88c897eae566231d
Niek <niekvandenbos@hotmail.com>
2020-04-04 18:31:01 +0200

    Add camelcase helper.

--- src/helpers.js
+++ src/helpers.js
@@ -1,1 +1,13 @@
 export const sluggify = string => string.toLowerCase().split(' ').join('-');
+
+export const toCamelCase = string => {
+  const casingFunction = (letter, index) => {
+    if (index === 0) {
+      return letter.toLowerCase();
+    }
+
+    return letter.toUpperCase();
+  };
+
+  return string.replace(/(?:^\w|[A-Z]|\b\w)/g, casingFunction).replace(/\s+/g, '')
+};
