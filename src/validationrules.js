15b4733c17a25028d4a393f867a0a4921a200898
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-12 16:04:49 +0200

    Fix numeric rule.

--- src/validationrules.js
+++ src/validationrules.js
@@ -2,6 +2,6 @@ export const required = (message = null) => value => !!value || message || 'This
 
 export const between = (lowerLimit, upperLimit, message = null) => value => (value >= lowerLimit && value <= upperLimit) || message || `The value must be between ${lowerLimit} and ${upperLimit}.`;
 
-export const numeric = (message = null) => value => (!isNaN(value) && !value.toString().includes('.')) || message || 'The value must be numeric';
+export const numeric = (message = null) => value => (!isNaN(value) && /^[0-9]*$/.test(value)) || message || 'The value must be numeric';
 
 export const positive = (message = null) => value => value >= 1 || message || 'The value must be positive';
