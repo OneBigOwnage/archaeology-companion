1a352a3c01634d84ba4d4ee1615c0392b4423d79
OneBigOwnage <niekvandenbos@hotmail.com>
2020-04-12 16:28:52 +0200

    Add validation to dig site add/edit.

--- src/components/CreateDigSiteDialog.vue
+++ src/components/CreateDigSiteDialog.vue
@@ -10,15 +10,27 @@
         </v-toolbar>
 
         <v-container>
-          <v-form>
+          <v-form v-model="isFormValid" ref="form">
             <v-row justify="center">
               <v-col sm="12" lg="4">
-                <v-text-field v-model="form.name" outlined label="Dig site name" required></v-text-field>
+                <v-text-field
+                  outlined
+                  label="Dig site name"
+                  required
+                  v-model="form.name"
+                  :rules="rules.name"
+                ></v-text-field>
               </v-col>
             </v-row>
             <v-row justify="center">
               <v-col sm="12" lg="4">
-                <v-text-field v-model="form.level" outlined label="Level requirement" required></v-text-field>
+                <v-text-field
+                  outlined
+                  label="Level requirement"
+                  required
+                  v-model="form.level"
+                  :rules="rules.name"
+                ></v-text-field>
               </v-col>
             </v-row>
 
@@ -38,19 +50,31 @@
 import DigSite from '@/models/digsite';
 import EventBus from '@/eventbus';
 import { v4 as uuidv4 } from 'uuid';
+import { required, numeric, between } from '@/validationrules';
 
 export default {
   data() {
     return {
       isOpen: false,
+      isFormValid: false,
       form: {
         name: null,
         level: null,
+      },
+      rules: {
+        name: [required()],
+        level: [required(), numeric(), between(1, 120)],
       },
     };
   },
   methods: {
     save() {
+      this.$refs.form.validate();
+
+      if (!this.isFormValid) {
+        return;
+      }
+
       const digSite = new DigSite(uuidv4(), this.form.name, this.form.level);
 
       this.$store.dispatch('digSites/add', digSite);
--- src/components/EditDigSiteDialog.vue
+++ src/components/EditDigSiteDialog.vue
@@ -10,16 +10,28 @@
         </v-toolbar>
 
         <v-container>
-          <v-form>
+          <v-form v-model="isFormValid" ref="form">
             <v-row justify="center">
               <v-col sm="12" lg="4">
-                <v-text-field v-model="form.name" outlined label="Dig site name" required></v-text-field>
+                <v-text-field
+                  outlined
+                  label="Dig site name"
+                  required
+                  v-model="form.name"
+                  :rules="rules.name"
+                ></v-text-field>
               </v-col>
             </v-row>
 
             <v-row justify="center">
               <v-col sm="12" lg="4">
-                <v-text-field v-model="form.level" outlined label="Level requirement" required></v-text-field>
+                <v-text-field
+                  outlined
+                  label="Level requirement"
+                  required
+                  v-model="form.level"
+                  :rules="rules.level"
+                ></v-text-field>
               </v-col>
             </v-row>
 
@@ -38,15 +50,21 @@
 <script>
 import DigSite from '@/models/digsite';
 import EventBus from '@/eventbus';
+import { required, numeric, between } from '@/validationrules';
 
 export default {
   data() {
     return {
       digSiteToUpdate: null,
       isOpen: false,
+      isFormValid: false,
       form: {
         name: null,
         level: null,
+      },
+      rules: {
+        name: [required()],
+        level: [required(), numeric(), between(1, 120)],
       },
     };
   },
@@ -56,6 +74,12 @@ export default {
       this.form.level = this.digSite.level;
     },
     save() {
+      this.$refs.form.validate();
+
+      if (!this.isFormValid) {
+        return;
+      }
+
       const isNameDiff = this.digSite.name !== this.form.name;
 
       this.digSiteToUpdate.name = this.form.name;
